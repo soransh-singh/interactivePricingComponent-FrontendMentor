a85741a67ba1ca689974b1fa134ffef346600737
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-06-06 11:54:25 +0530

    update challenge completed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+//document.documentElement.style.setProperty('--my-variable-name', 'pink');
 window.addEventListener("load", (e)=>{
   console.log("window loaded")
   const toggleBtn = document.querySelector("#toggle-btn")
@@ -9,32 +9,38 @@ window.addEventListener("load", (e)=>{
 
   const input = document.querySelector(".scroll")
   let prc
+  let width
   let isYearly = false
 
   function render(){
     if(input.value == 1){
       page.innerHTML = "10K"
       prc=8
+      width = "0%"
 
     } else if(input.value == 2){
       page.innerHTML = "50K"
       prc=12
+      width = "calc(25% - 10px)"
     } else if(input.value == 3){
       page.innerHTML = "100K"
       prc=16
+      width = "calc(50% - 20px)"
     }else if(input.value == 4){
       page.innerHTML = "500K"
       prc=24
+      width = "calc(75% - 30px)"
     }else if(input.value == 5){
       page.innerHTML = "1M"
       prc=36
+      width = "calc(100% - 40px)"
     }
     if(isYearly){
       prc = (prc*3)/4;
       console.log(prc);
     }
     price.innerHTML = `\$ ${prc}.00 `
-
+    document.documentElement.style.setProperty('--fill', width);
   }
 
 
